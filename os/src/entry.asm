c33a941534ce9c60bf54c1e0b3ce5ec20f8b5d19
kay <cn.kay.wang@gamil.com>
2023-11-08 20:17:02 +0800

    init commit

--- os/src/entry.asm
+++ os/src/entry.asm
@@ -1,4 +1,12 @@
-     .section .text.entry
-     .globl _start
- _start:
-     li x1, 100
+    .section .text.entry # 定义一个结点 .text 并指定为 entry(标识入口)
+    .globl _start # 定义一个全局符号 _start
+_start:
+    la sp, boot_stack_top
+    call main
+
+    .section .bss.stack
+    .globl boot_stack_lower_bound
+boot_stack_lower_bound:
+    .space 4096 * 16
+    .globl boot_stack_top
+boot_stack_top:
